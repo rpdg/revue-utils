a8cedbb432c32739f0e7181ea41f55b690f5da46
Lyu Pengfei <pf_lyu@qq.com>
2021-11-26 17:07:55 +0800

    add date format function

--- DateTime.ts
+++ DateTime.ts
@@ -15,6 +15,30 @@ export default {
 		let d = this.daySpan(dateFrom, dateTo);
 		return Math.ceil((d + dateFrom.getDay()) / 7);
 	},
+	format: function (date: Date, formatPatten: string = 'yyyy-MM-dd'): string {
+		let o = {
+			'M+': date.getMonth() + 1, //month
+			'd+': date.getDate(), //date
+			'h+': (date.getHours() > 12 ? date.getHours() - 12 : date.getHours()), //hour 12
+			'H+': date.getHours(), //hour 24
+			'm+': date.getMinutes(), //minute
+			's+': date.getSeconds(), //second
+			'q+': Math.floor((date.getMonth() + 3) / 3), //quarter
+			'S': date.getMilliseconds(), //millisecond
+		};
+
+		if (/(y+)/.test(formatPatten)) {
+			formatPatten = formatPatten.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+		}
+
+		for (let k in o) {
+			if (new RegExp('(' + k + ')').test(formatPatten)) {
+				formatPatten = formatPatten.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+			}
+		}
+
+		return formatPatten;
+	},
 };
 
 export const Hour = 3600e3;
