bf3d02ecb1d17d55810042286a6c767ba03c26ae
Lyu Pengfei <pf_lyu@qq.com>
2022-01-24 14:59:47 +0800

    add useExpose api

--- vue/use/index.ts
+++ vue/use/index.ts
@@ -1,3 +1,5 @@
 
 export * from './useClickAway';
 export * from './useEventListener';
+export * from './useExpose';
+
---
+++ vue/use/useExpose.ts
@@ -0,0 +1,10 @@
+import { getCurrentInstance } from "vue";
+
+// expose public api
+export function useExpose(apis: Record<string, any>) {
+    const instance = getCurrentInstance();
+    if (instance) {
+      Object.assign(instance.proxy, apis);
+    }
+  }
+  
