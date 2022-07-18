2839eed7b42f04b1ab1b57e475ba2fc86cadace6
Lyu Pengfei <pf_lyu@qq.com>
2022-07-18 16:56:08 +0800

    fix vue3 useExpose type error

--- vue/use/useExpose.ts
+++ vue/use/useExpose.ts
@@ -4,7 +4,7 @@ import { getCurrentInstance } from "vue";
 export function useExpose(apis: Record<string, any>) {
     const instance = getCurrentInstance();
     if (instance) {
-      Object.assign(instance.proxy, apis);
+      Object.assign(instance.proxy!, apis);
     }
   }
   
