87ff3e0ef9e4928ee302d9e45142bc15fe25a80f
Lyu Pengfei <pf_lyu@qq.com>
2023-11-02 09:37:43 +0800

    add File.downloadBlob

--- File.ts
+++ File.ts
@@ -109,3 +109,59 @@ function format_number(num: number, decimals: number, dec_point = ',', thousands
 	}
 	return s.join(dec);
 }
+
+/**
+ *
+ * @param blob  Blob
+ * @param filename  string
+ * @returns HTMLAnchorElement
+ *
+ * @example
+ *
+ * fetch('https://picsum.photos/list')
+  .then(response => response.json())
+  .then(data => data.filter(squareImages))
+  .then(collectionToCSV(exportFields))
+  .then(csv => {
+    const blob = new Blob([csv], { type: 'text/csv' });
+    downloadBlob(blob, 'photos.csv');
+  })
+  .catch(console.error);
+ */
+export function downloadBlob(blob: Blob, filename: string): HTMLAnchorElement {
+	// Create an object URL for the blob object
+	const url = URL.createObjectURL(blob);
+
+	// Create a new anchor element
+	const a = document.createElement('a');
+
+	// Set the href and download attributes for the anchor element
+	// You can optionally set other attributes like `title`, etc
+	// Especially, if the anchor element will be attached to the DOM
+	a.href = url;
+	a.download = filename || 'download';
+
+	// Click handler that releases the object URL after the element has been clicked
+	// This is required for one-off downloads of the blob content
+	const clickHandler = () => {
+		setTimeout(() => {
+			URL.revokeObjectURL(url);
+			removeEventListener('click', clickHandler);
+		}, 200);
+	};
+
+	// Add the click event listener on the anchor element
+	// Comment out this line if you don't want a one-off download of the blob content
+	a.addEventListener('click', clickHandler, false);
+
+	// Programmatically trigger a click on the anchor element
+	// Useful if you want the download to happen automatically
+	// Without attaching the anchor element to the DOM
+	// Comment out this line if you don't want an automatic download of the blob content
+	a.click();
+
+	// Return the anchor element
+	// Useful if you want a reference to the element
+	// in order to attach it to the DOM or use it in some other way
+	return a;
+}
