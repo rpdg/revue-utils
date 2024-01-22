ab81a2a07c89cbee7c206a11f4e93f8af678b744
Lyu Pengfei <pf_lyu@qq.com>
2024-01-22 15:28:40 +0800

    change export

--- Script.ts
+++ Script.ts
@@ -1,4 +1,4 @@
-export default function loadJs(src: string) {
+export function loadJs(src: string) {
 	return new Promise<void>((resolve, reject) => {
 		const script = document.createElement('script');
 		script.type = 'text/javascript';
